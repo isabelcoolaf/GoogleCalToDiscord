bf89bf25a850a530af05e095229c739bc91321dc
Riley <rileygardnerbowles@gmail.com>
2025-01-24 10:36:11 -0800

    Create Google auth input GUI

---
+++ src/main/java/edu/wsu/cs320/GoogleAuthWindow/GoogleAuthWindow.java
@@ -0,0 +1,13 @@
+package edu.wsu.cs320.GoogleAuthWindow;
+
+import javax.swing.*;
+
+public class GoogleAuthWindow {
+    public JPanel mainPanel;
+    private JButton saveButton;
+    private JPanel centerPanel;
+    private JLabel ifLabel;
+    private JTextField idField;
+    private JLabel secretLabel;
+    private JTextField secretField;
+}
