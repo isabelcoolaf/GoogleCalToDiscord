16d14a548fd33fdc6ef6e81d2532317cddce64d9
Isabel Lomas <me@isabel.gg>
2025-02-03 22:13:10 -0800

    generate GUI windows and open on start

--- src/main/java/edu/wsu/cs320/GoogleCalToDiscord.java
+++ src/main/java/edu/wsu/cs320/GoogleCalToDiscord.java
@@ -1,9 +1,21 @@
 package edu.wsu.cs320;
 
+import edu.wsu.cs320.gui.GoogleAuthWindow.GoogleAuthWindow;
+
+import javax.swing.*;
+
 public class GoogleCalToDiscord {
 
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        JFrame frame = new JFrame("Google Auth Window");
+        GoogleAuthWindow authWindow = new GoogleAuthWindow();
+
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setContentPane(authWindow.mainPanel);
+        frame.setSize(650, 150);
+        frame.setLocationRelativeTo(null);
+        frame.setVisible(true);
     }
 
 }
