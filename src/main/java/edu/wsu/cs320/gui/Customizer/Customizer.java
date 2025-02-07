57eac9c44447f38f80093a8a9a2c7c3cadd93e71
Riley <rileygardnerbowles@gmail.com>
2025-02-06 17:39:00 -0800

    Create basic Customizer GUI

--- src/main/java/edu/wsu/cs320/gui/Customizer/Customizer.java
+++ src/main/java/edu/wsu/cs320/gui/Customizer/Customizer.java
@@ -1,4 +1,28 @@
 package edu.wsu.cs320.gui.Customizer;
 
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
 public class Customizer {
+    private JPanel mainPanel;
+    private JPanel imagePreview;
+    private JPanel previewContainer;
+    private JProgressBar timeProgress;
+    private JLabel eventName;
+    private JLabel eventSummary;
+    private JLabel timeStart;
+    private JLabel timeEnd;
+    private JButton changeImageButton;
+    private JLabel usingLabel;
+    private JButton imageButton;
+
+    public Customizer() {
+        imageButton.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                System.out.println("Button");
+            }
+        });
+    }
 }
