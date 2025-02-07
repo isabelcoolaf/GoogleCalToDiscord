539506b4806aaff13781486327e99cdd7e879d9c
Riley <rileygardnerbowles@gmail.com>
2025-02-06 16:22:52 -0800

    Create basic structure for GuiController

--- src/main/java/edu/wsu/cs320/gui/control/GuiController.java
+++ src/main/java/edu/wsu/cs320/gui/control/GuiController.java
@@ -1,16 +1,55 @@
 package edu.wsu.cs320.gui.control;
 
+import javax.swing.*;
+
 public class GuiController {
 
-    public enum StateEnum{
+    public enum StateEnum {
         AUTH,
         SELECT,
         CUSTOMIZE
     }
 
     private StateEnum state;
+    private boolean guiSpawned = false;
+    private JFrame currentGUI = null;
+    // TODO: Make currentGUI its own class for JPanel management
 
     public GuiController() {
-
+        currentGUI = new JFrame();
+        currentGUI.setVisible(false);
     }
+
+
+    public void setState(StateEnum newState) {
+        if (guiSpawned) closeGUI();
+        state = newState;
+        if (guiSpawned) spawnGUI();
+    }
+
+    public StateEnum getState() {
+        return state;
+    }
+
+    public boolean hasGUIOpen() {
+        return guiSpawned;
+    }
+
+    public void spawnGUI() {
+        //TODO: method should spawn GUI based on state
+        currentGUI.setVisible(true);
+        guiSpawned = true;
+    }
+
+    public void closeGUI() {
+        //TODO: method should destroy its given JPanel, assuming it has one
+        currentGUI.setVisible(false);
+        guiSpawned = false;
+    }
+
+    public void freezeInput() {
+        //TODO: Lock user out of meaningful interaction with the GUI
+    }
+
+
 }
