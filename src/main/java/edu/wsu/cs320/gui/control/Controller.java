c74b5c1a5e855b39f420e874cfd7f89ba3c699a5
Riley <rileygardnerbowles@gmail.com>
2025-02-06 16:03:39 -0800

    Create new StateEnum within Controller Class

--- src/main/java/edu/wsu/cs320/gui/control/Controller.java
+++ src/main/java/edu/wsu/cs320/gui/control/Controller.java
@@ -2,6 +2,14 @@ package edu.wsu.cs320.gui.control;
 
 public class Controller {
 
+    public enum StateEnum{
+        AUTH,
+        SELECT,
+        CUSTOMIZE
+    }
+
+    private StateEnum state;
+
     public Controller() {
 
     }
