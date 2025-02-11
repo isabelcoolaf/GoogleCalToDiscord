b46ac6a1e7c3c63cec47935c7763d9f72ec0d091
Riley <rileygardnerbowles@gmail.com>
2025-02-11 14:46:41 -0800

    Fix visibility of main panel

--- src/main/java/edu/wsu/cs320/gui/calendar/CalendarSelector.java
+++ src/main/java/edu/wsu/cs320/gui/calendar/CalendarSelector.java
@@ -11,7 +11,7 @@ import java.awt.*;
 
 public class CalendarSelector {
     private JLabel selectCalendarLabel;
-    private JPanel mainPanel;
+    public JPanel mainPanel;
     private JPanel entryPanel;
     private CalendarSelectorButton[] entries;
     private ButtonGroup entryButtonGroup = new ButtonGroup();
