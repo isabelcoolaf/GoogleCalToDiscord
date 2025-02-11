288b9a2162e9257d6a52fcae4c8551cea1fb6008
Riley <rileygardnerbowles@gmail.com>
2025-02-11 15:47:14 -0800

    Add guiFrame field

--- src/main/java/edu/wsu/cs320/gui/control/ResponsiveGUI.java
+++ src/main/java/edu/wsu/cs320/gui/control/ResponsiveGUI.java
@@ -1,9 +1,12 @@
 package edu.wsu.cs320.gui.control;
 
+import javax.swing.*;
 import java.net.URI;
 import java.util.Calendar;
 
 public interface ResponsiveGUI {
+
+    JFrame guiFrame = null;
 
     default URI getURI() {
         return null;
