5386feeeda24966a78c37a58c3a69f45cca2857a
Riley <rileygardnerbowles@gmail.com>
2025-02-14 16:10:12 -0800

    move GoogleOAuth manager to main

--- src/main/java/edu/wsu/cs320/GoogleCalToDiscord.java
+++ src/main/java/edu/wsu/cs320/GoogleCalToDiscord.java
@@ -1,8 +1,11 @@
 package edu.wsu.cs320;
 
+import edu.wsu.cs320.googleapi.GoogleOAuthManager;
 import edu.wsu.cs320.gui.GoogleAuthWindow.GoogleAuthWindow;
+import edu.wsu.cs320.gui.control.GuiResponse;
 
 import javax.swing.*;
+import java.util.Arrays;
 
 public class GoogleCalToDiscord {
 
@@ -10,11 +13,41 @@ public class GoogleCalToDiscord {
         JFrame frame = new JFrame("Google Auth Window");
         GoogleAuthWindow authWindow = new GoogleAuthWindow();
 
+
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setContentPane(authWindow.mainPanel);
         frame.setSize(650, 150);
         frame.setLocationRelativeTo(null);
         frame.setVisible(true);
+
+
+        GuiResponse<String[]> resp;
+        while (true) {
+            resp = authWindow.getResponse();
+            if (resp.status == GuiResponse.ResponseCode.INCOMPLETE_DATA) {
+                System.out.println("Data incomplete");
+                continue;
+            }
+            if (resp.status == GuiResponse.ResponseCode.WINDOW_CLOSED) {
+                return;
+            }
+            if (resp.status == GuiResponse.ResponseCode.CANCELLED) {
+                return;
+            } else { // Response OK
+                break;
+            }
+        }
+        try {
+            GoogleOAuthManager manager = new GoogleOAuthManager(resp.data[0], resp.data[1], "");
+            manager.invokeFlow();
+        } catch (Exception e) {
+            // TODO: Handle errors with auth flow
+            // invokeFlow() will return an error with good text in it to display.
+            // the function also blocks until complete, so keep that in mind
+        }
+
+        System.out.println("code: " + resp.status + " data: " + Arrays.toString(resp.data));
+
     }
 
 }
