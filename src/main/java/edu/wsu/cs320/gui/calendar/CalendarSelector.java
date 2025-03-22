6a25a23d1110c1bb019035bb40b554bb2a7458f1
Isabel Lomas <me@isabel.gg>
2025-03-21 23:49:02 -0700

    regenerate GUI

--- src/main/java/edu/wsu/cs320/gui/calendar/CalendarSelector.java
+++ src/main/java/edu/wsu/cs320/gui/calendar/CalendarSelector.java
@@ -122,11 +122,11 @@ public class CalendarSelector implements ResponsiveGUI {
         confirmButton = new JButton();
         confirmButton.setText("Confirm");
         mainPanel.add(confirmButton, new GridConstraints(2, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_HORIZONTAL, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_CAN_GROW, GridConstraints.SIZEPOLICY_FIXED, null, null, null, 0, false));
-        final JScrollPane scrollPane1 = new JScrollPane();
-        mainPanel.add(scrollPane1, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
+        scrollPane = new JScrollPane();
+        mainPanel.add(scrollPane, new GridConstraints(1, 0, 1, 1, GridConstraints.ANCHOR_CENTER, GridConstraints.FILL_BOTH, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, GridConstraints.SIZEPOLICY_CAN_SHRINK | GridConstraints.SIZEPOLICY_WANT_GROW, null, null, null, 0, false));
         entryPanel = new JPanel();
         entryPanel.setLayout(new GridLayoutManager(1, 1, new Insets(0, 0, 0, 0), -1, -1));
-        scrollPane1.setViewportView(entryPanel);
+        scrollPane.setViewportView(entryPanel);
     }
 
     /**
